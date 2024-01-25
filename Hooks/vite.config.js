10eff8157ce1fc312bca02d90466242ad19a445f
Arghadeep <arghadeeps07@gmail.com>
2024-01-25 23:28:50 +0530

    Commited all files...

--- Hooks/vite.config.js
+++ Hooks/vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+export default {
+  base: '/React/Hooks/', // for "React" repository in "Hooks" subdirectory
+}
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
